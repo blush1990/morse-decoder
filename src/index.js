074a359ef8d193cbfa18f60987f04af88ba088ab
blush1990 <evgenypopov16051995@gmail.com>
2022-07-15 19:11:48 +0300

    feat: task finish

--- src/index.js
+++ src/index.js
@@ -35,11 +35,46 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
+
+
 function decode(expr) {
-    // write your solution here
+    let arrWord = [];
+    let arrSimbol = [];
+    let arrSimbolSecond = [];
+    let res = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arrWord.push(expr.substr(i, 10))
+    }
+    for (let j = 0; j < arrWord.length; j++) {
+        for (let k = 0; k < 10; k += 2) {
+            if (arrWord[j].substr(k, 2) === "10") {
+                arrSimbol.push('.');
+            }
+            else if (arrWord[j].substr(k, 2) === "11") {
+                arrSimbol.push('-');
+            }
+            else if (arrWord[j].substr(k, 10) === "**********") {
+                arrSimbol.push(' ');
+            }
+        }
+        arrSimbol.push(','); 
+    }
+    arrSimbol = arrSimbol.join('').split(',')
+    
+    for (let l = 0; l < arrSimbol.length; l++) {
+        for (let key in MORSE_TABLE) {  
+            if (arrSimbol[l] === key) {
+                res.push(MORSE_TABLE[key])
+            };
+        };
+    };
+   
+    return res.join('')
 }
+
 
 module.exports = {
     decode
